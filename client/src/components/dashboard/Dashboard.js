52d010f75bee477d72bd7de8be937d94a8b455c2
vedhagiri <giri@brewengage.com>
2020-08-08 15:13:40 +0530

    Added spinner component.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
 
-const Dashboard = ({getCurrentProfile, auth, profile}) => {
+const Dashboard = ({getCurrentProfile, auth, profile: { profile, loading}}) => {
 
     useEffect(() => {
         getCurrentProfile();
     }, []);
 
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+    return loading && profile === null ? <Spinner/> : <Fragment>
+        Test
+    </Fragment>
 }
 
 Dashboard.propTypes = {
