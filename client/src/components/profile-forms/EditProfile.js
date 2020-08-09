6b754aea0a6a51f72c811709af2f28b0c8886354
vedhagiri <giri@brewengage.com>
2020-08-09 10:57:26 +0530

    Profile add experience and education.

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -167,7 +167,7 @@ const EditProfile = ({profile:{profile, loading}, createProfile, getCurrentProfi
                 </Fragment>}        
             
             <input type="submit" className="btn btn-primary my-1" />
-            <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
+            <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
         </form>
         </Fragment>
     )
