c2894e6610000290b00446337b159216052ef213
vedhagiri <giri@brewengage.com>
2020-08-09 16:04:24 +0530

    load developer profile by id.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -51,11 +51,11 @@ export const getProfiles = () => async dispatch => {
 
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
-    dispatch({type: CLEAR_PROFILE});
     try {
         const res = await axios.get(`/api/profile/user/${userId}`);
+        console.log(res);
         dispatch({
-            type: GET_PROFILES,
+            type: GET_PROFILE,
             payload: res.data
         });
     } catch (err) {
