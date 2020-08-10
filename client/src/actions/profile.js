3be3b4699cdd9ea273ba160c91b3961328caf4fe
vedhagiri <giri@brewengage.com>
2020-08-10 05:32:24 +0530

    github repos route and profile changes.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -70,7 +70,6 @@ export const getProfileById = userId => async dispatch => {
 
 // Get Github repos
 export const getGithubRepos = username => async dispatch => {
-    dispatch({type: CLEAR_PROFILE});
     try {
         const res = await axios.get(`/api/profile/github/${username}`);
         dispatch({
